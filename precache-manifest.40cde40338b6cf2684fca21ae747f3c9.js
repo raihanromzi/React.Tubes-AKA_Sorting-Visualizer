5b70b33eb42b741c4e98ff2a394d408be3b943d9
raihanromzi <raihan.rakhman16@gmail.com>
2020-12-20 06:52:17 +0700

    Updates

---
+++ precache-manifest.40cde40338b6cf2684fca21ae747f3c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8032597652d4cb4d4c85a90b0b6d8e0b",
+    "url": "/web-tubes-aka/index.html"
+  },
+  {
+    "revision": "901051b2f7e241fd73f2",
+    "url": "/web-tubes-aka/static/css/main.868e2091.chunk.css"
+  },
+  {
+    "revision": "c9801371aa319f7e39dd",
+    "url": "/web-tubes-aka/static/js/2.0caf21e4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/web-tubes-aka/static/js/2.0caf21e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901051b2f7e241fd73f2",
+    "url": "/web-tubes-aka/static/js/main.053b3ce5.chunk.js"
+  },
+  {
+    "revision": "b3614ea1a431a18ea8a3",
+    "url": "/web-tubes-aka/static/js/runtime-main.f9a58c99.js"
+  }
+]);
